698c434e29ed61b8f29c2b578a8b9e7c7c23948e
Jay Garcia <jesusdgarcia@gmail.com>
2015-04-01 09:46:21 -0400

    Pre-publish push

---
+++ Step1/iOS/MCFileWriterUtil.m
@@ -0,0 +1,41 @@
+//
+//  MCFileWriterUtil.m
+//  CustomComponent
+//
+//  Created by Jesus Garcia on 3/30/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import "MCFileWriterUtil.h"
+
+@implementation MCFileWriterUtil
+
+
+// Persist data
+- (void) writeFile:(NSString *)fileName
+            withContents:(NSString *)contents
+            errorCallback:(RCTResponseSenderBlock)failureCallback
+            callback:(RCTResponseSenderBlock)successCallback {
+    
+    // #Step 1
+    RCT_EXPORT();
+    NSLog(@"%@ %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+
+    successCallback(@[@"Write method called"]);
+}
+
+            
+// Load data from disk and return the String.
+- (void) readFile:(NSString *)fileName
+            errorCallback:(RCTResponseSenderBlock)failureCallback
+            callback:(__unused RCTResponseSenderBlock)successCallback {
+
+    // #Step 1
+    RCT_EXPORT();
+    NSLog(@"%@ %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+    
+    successCallback(@[@"Read method called"]);
+
+}
+
+@end
---
+++ Step2/README.md
@@ -0,0 +1,11 @@
+# custom-react-native-components-part1
+
+This repo is created for an article that Jay Garcia is writing for ModusCreate (http://Moduscreate.com). 
+
+# Getting started
+- Be sure you have NodeJS & NPM installed on your system
+- Clone this repo
+- Open CustomComponent.xcodeproj in XCode
+
+# Thigns to look at
+- jsx/* 
